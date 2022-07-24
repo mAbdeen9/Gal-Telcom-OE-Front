098b04ee14547a79f4870dd53eba14b408a7d09d
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-07-24 21:07:33 +0300

    menu style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import { useState } from "react";
+import { Fragment } from "react";
 import classes from "./Header.module.css";
 import Logo from "./Logo";
+import Menu from "./Menu";
+
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleBurger = () => {
+    setShowMenu((state) => !state);
+  };
   return (
-    <div className={`container-fluid ${classes.navBox}`}>
-      <div className="container">
-        <div className={classes.nav}>
-          <div className={classes.boxBurger}>
-            <i className="bi bi-list"> </i>
-          </div>
-          <div className={classes.box}>
-            ברוך הבא מוחמד
-            <span className={classes.emoji} role="img" aria-label="hi emoji">
-              👋🏻
-            </span>{" "}
-          </div>
-          <div className={classes.box}>
-            <Logo class={classes.logo} />
+    <Fragment>
+      <div className={`container-fluid ${classes.navBox}`}>
+        <div className="container">
+          <div className={classes.nav}>
+            <div onClick={toggleBurger} className={classes.boxBurger}>
+              <i className="bi bi-list"> </i>
+            </div>
+            <div className={classes.box}>
+              ברוך הבא מוחמד
+              <span className={classes.emoji} role="img" aria-label="hi emoji">
+                👋🏻
+              </span>
+            </div>
+            <div className={classes.box}>
+              <Logo class={classes.logo} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {showMenu && <Menu></Menu>}
+      <div className={classes.test}>Helloo</div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Menu.module.css";
+function Menu() {
+  return (
+    <div className={`container-fluid ${classes.box}`}>
+      <div className="container">Menu</div>
+    </div>
+  );
+}
+
+export default Menu;
