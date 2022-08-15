dd55164ebb34a15773cd4ccb75b448b95464db24
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-08-15 20:01:37 +0300

    css updates

--- src/components/AdminControl/AdminControl.jsx
+++ src/components/AdminControl/AdminControl.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { useRef } from "react";
 import Container from "../Container/Container";
 import classes from "./AdminControl.module.css";
 
 function AdminControl() {
+  const usernameRef = useRef();
+  const phoneNumberRef = useRef();
+  const storeNumberRef = useRef();
+  const passwordRef = useRef();
+  const deletePhoneRef = useRef();
+
+  const creatNewUser = (e) => {
+    e.preventDefault();
+    const newUser = {
+      username: usernameRef.current.value,
+      phoneNumber: phoneNumberRef.current.value,
+      storeNumber: storeNumberRef.current.value,
+      password: passwordRef.current.value,
+    };
+    console.log(newUser);
+  };
+
+  const deleteUser = (e) => {
+    e.preventDefault();
+    const userPhoneNum = deletePhoneRef.current.value;
+    console.log(userPhoneNum);
+  };
+
   return (
     <Container>
       <div className={classes.admin_panel_main}>
-        <form>
+        <form onSubmit={creatNewUser}>
           <div className={classes.card}>
             <h4 className={classes.t1}>
               <i className="bi bi-person-plus"></i> משתמש חדש
@@ -18,26 +42,26 @@ function AdminControl() {
             </div>
             <div className={classes.input_box}>
               <label htmlFor="userName">שם משתמש : </label>{" "}
-              <input id="userName" type="text" />
+              <input ref={usernameRef} id="userName" type="text" />
             </div>
             <div className={classes.input_box}>
               <label htmlFor="phoneNumber">מספר טלפון : </label>{" "}
-              <input id="phoneNumber" type="number" />
+              <input ref={phoneNumberRef} id="phoneNumber" type="number" />
             </div>
             <div className={classes.input_box}>
               <label htmlFor="store">מספר מחסן : </label>{" "}
-              <input id="store" type="number" />
+              <input ref={storeNumberRef} id="store" type="number" />
             </div>
             <div className={classes.input_box}>
               <label htmlFor="password">סיסמה : </label>{" "}
-              <input id="password" type="number" />
+              <input ref={passwordRef} id="password" type="number" />
             </div>
             <div>
               <button className={classes.done}>בוצע</button>
             </div>
           </div>
         </form>
-        <form>
+        <form onSubmit={deleteUser}>
           <div className={classes.card}>
             <h4 className={classes.t1}>
               <i className="bi bi-person-x"></i> מחיקת משתמש
@@ -50,7 +74,7 @@ function AdminControl() {
             <div className={classes.input_box}></div>
             <div className={classes.input_box}>
               <label htmlFor="phoneNumber">מספר טלפון : </label>{" "}
-              <input id="phoneNumber" type="number" />
+              <input ref={deletePhoneRef} id="phoneNumber" type="number" />
             </div>
             <div className={classes.input_box}></div>
             <div className={classes.input_box}></div>
