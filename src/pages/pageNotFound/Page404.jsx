51edd1610452fb00e1b3ab0d301257836822405e
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-08-18 17:38:43 +0300

    404 page add

--- src/pages/pageNotFound/Page404.jsx
+++ src/pages/pageNotFound/Page404.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-
+import classes from "./pageNotFound.module.css";
 function Page404() {
   return (
-    <div>
-      page not found{" "}
-      <span aria-label="error" role="img">
-        💥
-      </span>
+    <div className={classes.box}>
+      <figure>
+        <div className={classes.sad_mac}></div>
+        <figcaption>
+          <span className={classes.sr_text}>Error 404: Not Found</span>
+          <span className={classes.e}></span>
+          <span className={classes.r}></span>
+          <span className={classes.r}></span>
+          <span className={classes.o}></span>
+          <span className={classes.r}></span>
+          <span className={classes._4}></span>
+          <span className={classes._0}></span>
+          <span className={classes._4}></span>
+          <span className={classes.n}></span>
+          <span className={classes.o}></span>
+          <span className={classes.t}></span>
+          <span className={classes.f}></span>
+          <span className={classes.o}></span>
+          <span className={classes.u}></span>
+          <span className={classes.n}></span>
+          <span className={classes.d}></span>
+        </figcaption>
+      </figure>
     </div>
   );
 }
